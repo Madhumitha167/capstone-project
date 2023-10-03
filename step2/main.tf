b1dfefb6c28452218c6d8379b13aef2dfda5a439
Madhumitha167 <118745539+Madhumitha167@users.noreply.github.com>
2023-10-03 23:52:04 +0530

    Delete step2 directory

--- step2/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ami_from_instance" "CAPSTONE" {
-  name               = "CAPSTONE"
-  source_instance_id = var.source_instance_id
-}
--- step2/var.tf
+++
@@ -1,5 +0,0 @@
-variable "source_instance_id" {
-  description = "The ID of the source AWS EC2 instance from which to create the AMI."
-  type        = string
-  default     = "" 
-}
