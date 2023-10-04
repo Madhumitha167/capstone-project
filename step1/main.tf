11accf85056db73d9926bc4de51ec45e46e03ff7
Madhumitha167 <118745539+Madhumitha167@users.noreply.github.com>
2023-10-04 09:14:45 +0530

    Update main.tf

--- step1/main.tf
+++ step1/main.tf
@@ -81,31 +81,13 @@ resource "aws_security_group" "madhu_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-#Creating a keypair
-
-resource "aws_key_pair" "new_key" {
-  key_name   = "new_key"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "new_key" {
-    content  = tls_private_key.rsa.private_key_pem
-    filename = "newkey"
-}
-
 #Instance 
 
 resource "aws_instance" "myinstance" {
   ami           = var.ami_id 
   instance_type = var.instance_type     
   subnet_id     = aws_subnet.madhu_subnet1.id
-  key_name      = "new_key"
+  key_name      = "kubeadm"
   security_groups = [aws_security_group.madhu_sg.id]
   tags = {
     Name = "webserver-terr"  
